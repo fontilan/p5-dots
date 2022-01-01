b04963bc755fdf87af4a044b20f937d3af3a202b
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-01 18:09:29 +0100

    remove unnecessary stuff

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
 function setup() {
   windowSize = min(windowWidth * 0.6, windowHeight * 0.6);
   createCanvas(windowSize, windowSize);
-  let x = windowWidth;
-  let y = windowHeight;
-  console.log("windowSize:", windowSize);
 }
 
 function draw() {
@@ -12,7 +9,7 @@ function draw() {
     for (let j = 10; j < Math.floor(windowSize); j = j + windowSize / 29) {
       strokeWeight(random(windowSize / 175, windowSize / 35));
       stroke(random(0, 150), 26, 105);
-      point(j, i);
+      point(i, j);
     }
   }
   noLoop();
