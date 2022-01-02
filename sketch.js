cdab35e25687ea896c22b6c1431f73ea2b1630f8
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-02 13:30:22 +0100

    random colour now chosen from a prefixed array

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
   canvasSize = min(windowWidth * 0.5, windowHeight * 0.5);
   createCanvas(canvasSize, canvasSize);
+  noLoop();
 }
+
+let colours = [
+  "#e6dfff",
+  "#d9bfff",
+  "#cc9eff",
+  "#c588ff",
+  "#ac71ff",
+  "#9059ff",
+  "#7543e3",
+  "#582acb",
+  "#46278e",
+  "#321c64",
+];
 
 // Just dots
 //
@@ -9,11 +23,10 @@ function draw() {
   for (let i = 10; i < canvasSize * 0.98; i = i + canvasSize / 29) {
     for (let j = 10; j < canvasSize * 0.98; j = j + canvasSize / 29) {
       strokeWeight(random(canvasSize / 175, canvasSize / 35));
-      stroke(random(0, 175), 26, 105);
+      stroke(random(colours));
       point(i, j);
     }
   }
-  noLoop();
 }
 
 //Hanging beads
@@ -22,11 +35,10 @@ function draw() {
 //   for (let i = 10; i < canvasSize * 0.99; i = i + canvasSize / 39) {
 //     for (let j = 10; j < canvasSize * 0.99; j = j + canvasSize / 99) {
 //       strokeWeight(random(canvasSize / 105, canvasSize / 45));
-//       stroke(random(0, 175), 26, 105);
+//       stroke(random(colours));
 //       point(i, j);
 //     }
 //   }
-//   noLoop();
 // }
 
 // Overgrown
@@ -35,11 +47,10 @@ function draw() {
 //   for (let i = 10; i < canvasSize * 0.99; i = i + canvasSize / 99) {
 //     for (let j = 10; j < canvasSize * 0.99; j = j + canvasSize / 99) {
 //       strokeWeight(random(canvasSize / 195, canvasSize / 45));
-//       stroke(random(0, 175), 26, 105);
+//       stroke(random(colours));
 //       point(i, j);
 //     }
 //   }
-//   noLoop();
 // }
 
 // Fluffy square
@@ -48,11 +59,10 @@ function draw() {
 //   for (let i = 20; i < canvasSize * 0.97; i = i + canvasSize / 75) {
 //     for (let j = 20; j < canvasSize * 0.97; j = j + canvasSize / 75) {
 //       strokeWeight(random(canvasSize / 80, canvasSize / 20));
-//       stroke(random(0, 175), 26, 105);
+//       stroke(random(colours));
 //       point(j, i);
 //     }
 //   }
-//   noLoop();
 // }
 
 // Sea of Purple
@@ -61,11 +71,10 @@ function draw() {
 //   for (let i = 10; i < canvasSize + 100; i = i + canvasSize / 99) {
 //     for (let j = 10; j < canvasSize * 0.99; j = j + canvasSize / 99) {
 //       strokeWeight(random(canvasSize / 80, canvasSize / 5));
-//       stroke(random(0, 175), 26, 105);
+//       stroke(random(colours));
 //       point(j, i);
 //     }
 //   }
-//   noLoop();
 // }
 
 // Triangle of cirlces
@@ -75,9 +84,8 @@ function draw() {
 //   for (let i = 40; i < canvasSize; i = i + canvasSize / 29) {
 //     for (let j = 0; j < canvasSize + 400; j = j + canvasSize / 29) {
 //       strokeWeight(random(canvasSize / 80, canvasSize / 5));
-//       stroke(random(0, 175), 26, 105);
+//       stroke(random(colours));
 //       point(j, i);
 //     }
 //   }
-//   noLoop();
 // }
