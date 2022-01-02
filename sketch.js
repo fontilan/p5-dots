35a34325c50caae6be1ad346e41bc696bc1ccc9a
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-02 12:27:07 +0100

    rename windowSize to canvasSize

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-  windowSize = min(windowWidth * 0.5, windowHeight * 0.5);
-  createCanvas(windowSize, windowSize);
+  canvasSize = min(windowWidth * 0.5, windowHeight * 0.5);
+  createCanvas(canvasSize, canvasSize);
 }
 
 // Just dots
 //
 function draw() {
-  for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 29) {
-    for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 29) {
-      strokeWeight(random(windowSize / 175, windowSize / 35));
+  for (let i = 10; i < canvasSize * 0.99; i = i + canvasSize / 29) {
+    for (let j = 10; j < canvasSize * 0.99; j = j + canvasSize / 29) {
+      strokeWeight(random(canvasSize / 175, canvasSize / 35));
       stroke(random(0, 150), 26, 105);
       point(i, j);
     }
@@ -19,9 +19,9 @@ function draw() {
 //Hanging beads
 //
 // function draw() {
-//   for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 39) {
-//     for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 99) {
-//       strokeWeight(random(windowSize / 105, windowSize / 45));
+//   for (let i = 10; i < canvasSize * 0.99; i = i + canvasSize / 39) {
+//     for (let j = 10; j < canvasSize * 0.99; j = j + canvasSize / 99) {
+//       strokeWeight(random(canvasSize / 105, canvasSize / 45));
 //       stroke(random(0, 150), 26, 105);
 //       point(i, j);
 //     }
@@ -32,9 +32,9 @@ function draw() {
 // Overgrown
 //
 // function draw() {
-//   for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 99) {
-//     for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 99) {
-//       strokeWeight(random(windowSize / 195, windowSize / 45));
+//   for (let i = 10; i < canvasSize * 0.99; i = i + canvasSize / 99) {
+//     for (let j = 10; j < canvasSize * 0.99; j = j + canvasSize / 99) {
+//       strokeWeight(random(canvasSize / 195, canvasSize / 45));
 //       stroke(random(0, 150), 26, 105);
 //       point(i, j);
 //     }
@@ -45,9 +45,9 @@ function draw() {
 // Fluffy square
 //
 // function draw() {
-//   for (let i = 20; i < windowSize * 0.97; i = i + windowSize / 75) {
-//     for (let j = 20; j < windowSize * 0.97; j = j + windowSize / 75) {
-//       strokeWeight(random(windowSize / 80, windowSize / 20));
+//   for (let i = 20; i < canvasSize * 0.97; i = i + canvasSize / 75) {
+//     for (let j = 20; j < canvasSize * 0.97; j = j + canvasSize / 75) {
+//       strokeWeight(random(canvasSize / 80, canvasSize / 20));
 //       stroke(random(0, 150), 26, 105);
 //       point(i, j);
 //     }
