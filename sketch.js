6bf43b19c731cb7025e03821adfd469b85c790a0
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-02 11:52:03 +0100

    include all 4 variants

--- sketch.js
+++ sketch.js
@@ -1,13 +1,53 @@
 function setup() {
-  windowSize = min(windowWidth * 0.75, windowHeight * 0.75);
+  windowSize = min(windowWidth * 0.5, windowHeight * 0.5);
   createCanvas(windowSize, windowSize);
 }
 
+// Just dots
+//
+// function draw() {
+//   for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 29) {
+//     for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 29) {
+//       strokeWeight(random(windowSize / 175, windowSize / 35));
+//       stroke(random(0, 150), 26, 105);
+//       point(i, j);
+//     }
+//   }
+//   noLoop();
+// }
+
+//Hanging beads
+//
+// function draw() {
+//   for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 39) {
+//     for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 99) {
+//       strokeWeight(random(windowSize / 105, windowSize / 45));
+//       stroke(random(0, 150), 26, 105);
+//       point(i, j);
+//     }
+//   }
+//   noLoop();
+// }
+
+// Overgrown
+//
+// function draw() {
+//   for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 99) {
+//     for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 99) {
+//       strokeWeight(random(windowSize / 195, windowSize / 45));
+//       stroke(random(0, 150), 26, 105);
+//       point(i, j);
+//     }
+//   }
+//   noLoop();
+// }
+
+// Fluffy square
+//
 function draw() {
-  background(240);
-  for (let i = 10; i < windowSize * 0.99; i = i + windowSize / 39) {
-    for (let j = 10; j < windowSize * 0.99; j = j + windowSize / 99) {
-      strokeWeight(random(windowSize / 105, windowSize / 45));
+  for (let i = 20; i < windowSize * 0.97; i = i + windowSize / 75) {
+    for (let j = 20; j < windowSize * 0.97; j = j + windowSize / 75) {
+      strokeWeight(random(windowSize / 80, windowSize / 20));
       stroke(random(0, 150), 26, 105);
       point(i, j);
     }
